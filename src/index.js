8a1984016b27420c13b877ee9d4eac60cfce6228
Pavel Shlapak <letohx@gmail.com>
2023-08-12 17:58:33 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsPair = [];
+  let currentIndex  = 0;
+
+  bracketsConfig.forEach((item) => bracketsPair.push(item[0] + item[1]));
+
+  while (bracketsPair.length > currentIndex) {
+      if (str.includes(bracketsPair[currentIndex])) {
+          str = str.replace(bracketsPair[currentIndex], '');
+          currentIndex = 0;
+      } else {
+        currentIndex++;
+      }
+  }
+
+  return str === '';
 }
